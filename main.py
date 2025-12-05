c37e3abbaf94ac98c12e72e665cdd2574595863d
owen.kesterson5510@gmail.com <you@example.com>
2025-12-05 14:14:29 -0500

    britanica

--- main.py
+++ main.py
@@ -1,26 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 
-
 url = input("Enter a URL:\n")
-response = requests.get(url) #gets the html of the url
-soupedtext = BeautifulSoup(response.text, 'html.parser') #makes the html text able to be souped/run functions on
+payload = {}
+headers = {}
 
-newsoupedtext = soupedtext.find_all("dd", attrs={"data-sentry-source-file": "box.tsx"}) #scans the entire html and grabs only the citation box
-nst0 = newsoupedtext
-nst1 = [ ]
-for element in nst0: #repeatedly goes through all the citations and pulls out only the text
-    text = element.get_text() 
-    nst1.append(text)
-ArticleName = nst1[0]
-AuthorsName = nst1[1]
-WebsiteName = nst1[2]
-URL = nst1[3]
-DateAccessed = nst1[4]
-Publisher = nst1[5]
-DateLastUpdate = nst1[6]
-Heresthecitation = "Here's the citation: \n"
-print(f'{Heresthecitation}{AuthorsName}. \"{ArticleName}.\" {WebsiteName}. {Publisher}, {DateLastUpdate}. Web. Accessed {DateAccessed}.\n<{URL}>')
+response = requests.request("GET", url, headers=headers, data=payload)
+soupedtext = BeautifulSoup(response.text, 'html.parser') # ------- makes the html text able to be souped/run functions on ---------
+if "www.history.com" in str(url): # ------ runs this for HISTORY.COM articles... -----
+    newsoupedtext = soupedtext.find_all("dd", attrs={"data-sentry-source-file": "box.tsx"}) #scans the entire html and grabs only the citation box
+    nst0 = newsoupedtext
+    nst1 = [ ]
+    for element in nst0: # -------- repeatedly goes through all the citations and pulls out only the text, formatts it as a list -----------
+        text = element.get_text() 
+        nst1.append(text)
+    # -------- these commands all define certain portions of the list as different data -----
+    ArticleName = nst1[0]
+    AuthorsName = nst1[1]
+    WebsiteName = nst1[2]
+    URL = nst1[3]
+    DateAccessed = nst1[4]
+    Publisher = nst1[5]
+    DateLastUpdate = nst1[6]
+    Heresthecitation = "Here's the citation: \n"
+    # ------- the final print ----------
+    print(f'{Heresthecitation}{AuthorsName}. \"{ArticleName}.\" {WebsiteName}. {Publisher}, {DateLastUpdate}. Web. Accessed {DateAccessed}.\n<{URL}>')
+elif "www.britannica.com" in str(url): # ------ runs this for BRITANNICA.COM articles... -----
+    nst2 = soupedtext.find_all("div", attrs={"class": "citation font-serif border rounded p-15 mt-20"})
+    print(nst2)
+
 
 
 
